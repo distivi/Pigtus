427d61cdf2eb1506cda628036f57617862c21d60
barchinsky <barchinsky@gmail.com>
2018-03-07 18:53:53 +0200

    Feature/feature_2: Added media and header component  - added backgroung media;  - created Header skeleton;  - updated some styles;

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,13 +5,9 @@ class App extends Component {
 	render() {
 		return (
 			<div className="app">
-				<header className="app-header">
-					<img src={logo} className="app-logo" alt="logo" />
-					<h1 className="app-title">Welcome to React</h1>
-				</header>
-				<p className="app-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
+				<div className="backdrop">
+					Pigtus
+				</div>
 			</div>
 		);
 	}
