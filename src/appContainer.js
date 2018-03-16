fdb064650b24f867478ea10b7a21cc4c1cbb4c21
barchinsky <barchinsky@gmail.com>
2018-03-16 16:48:16 +0200

    Features/feature_10: Added redux into project - added initial state; - added redux provider;

--- src/appContainer.js
+++ src/appContainer.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import App from './components/App/App';
+import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
 
 class AppContainer extends React.Component {
+  renderLoader = () => {
+    return (<div> Loading app </div>);
+  }
+
+  renderApp = () => {
+    return (<App {...this.props}/>);
+  }
+
   render() {
+    const view = this.props.loaded ?
+      this.renderApp() :
+      this.renderLoader();
+
     return (
       <div className="app-container">
-          <App {...this.props}/>
+          {view}
       </div>
     );
   }
 }
 
-export default withRouter(AppContainer);
+AppContainer.propTypes = {
+  loaded: PropTypes.bool.isRequired
+};
+
+const mapStateToProps = (state) => {
+  return {
+    loaded: state.loaded
+  };
+};
+
+export default withRouter(connect(mapStateToProps)(AppContainer));
